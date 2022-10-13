cac888fa284931a944e9e1f93a1ecd346f4ecaba
Oscar <oscar.montes201220@gmail.com>
2022-10-12 22:38:29 -0500

    math.c and math.h

---
+++ math.c
@@ -0,0 +1,7 @@
+/* funciones matematicas para nuestro codigo */
+int dobleInt (int a){
+	if (a > MAX_INT) {
+	return -1;
+	}
+	return a*2;
+}
---
+++ math.h
@@ -0,0 +1,3 @@
+/* Encabezados de Math.c */
+#define MAX_INT (2>sizeof(int))/2
+int dobleInt(int a);
